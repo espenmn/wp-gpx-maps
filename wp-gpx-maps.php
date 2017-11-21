29ad5cad1c58b2f8a3524bf4b54d1ed30af3a2f3
Espen Moe-Nilssen <espen@medialog.no>
2017-11-21 13:18:07 +0100

    red button for export GPX-data

--- wp-gpx-maps.php
+++ wp-gpx-maps.php
@@ -728,7 +728,7 @@ function handle_WP_GPX_Maps_Shortcodes($attr, $content='')
 			$dummy = ( defined('WP_SITEURL') ) ? WP_SITEURL : get_bloginfo('url');
 			$gpxurl = $dummy.$gpxurl;
 		}
-		$output.="<a href='$gpxurl' target='_new' download>".__("Download", "wp-gpx-maps")."</a>";
+		$output.="<a class='button btn' href='$gpxurl' target='_new' download>".__("Last ned GPX-data", "wp-gpx-maps")."</a>";
 	}
 
 	return $output;
